9a7260fe906a422a7802562ba601559017bc8688
Jonathan LELIEVRE <jonathan.lelievre@prestashop.com>
2025-07-22 16:26:23 +0200

    Add each company contribution percent

--- src/DTO/Company.php
+++ src/DTO/Company.php
@@ -6,6 +6,7 @@ class Company
 {
     public int $associatedPullRequests = 0;
     public int $rank = 0;
+    public float $contributionPercent = 0;
 
     public function __construct(
         public readonly string $name,
@@ -23,7 +24,7 @@ class Company
     }
 
     /**
-     * @return array<string, string|int>
+     * @return array<string, string|int|float>
      */
     public function toArray(): array
     {
@@ -31,6 +32,7 @@ class Company
             'name' => $this->name,
             'rank' => $this->rank,
             'contributions' => $this->associatedPullRequests,
+            'contribution_percent' => $this->contributionPercent,
             'avatar_url' => $this->avatarUrl,
             'html_url' => $this->htmlUrl,
         ];
