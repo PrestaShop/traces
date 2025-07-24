493062273beb6a94fe41318aa94edf5f38adbe05
Jonathan LELIEVRE <jonathan.lelievre@prestashop.com>
2025-07-24 18:27:43 +0200

    Export community data with companies

--- src/DTO/Company.php
+++ src/DTO/Company.php
@@ -4,9 +4,9 @@ namespace PrestaShop\Traces\DTO;
 
 class Company
 {
-    public int $associatedPullRequests = 0;
+    public int $mergedPullRequests = 0;
     public int $rank = 0;
-    public float $contributionPercent = 0;
+    public float $pullRequestsPercent = 0;
 
     public function __construct(
         public readonly string $name,
@@ -31,8 +31,8 @@ class Company
         return [
             'name' => $this->name,
             'rank' => $this->rank,
-            'contributions' => $this->associatedPullRequests,
-            'contribution_percent' => $this->contributionPercent,
+            'merged_pull_requests' => $this->mergedPullRequests,
+            'pull_requests_percent' => $this->pullRequestsPercent,
             'avatar_url' => $this->avatarUrl,
             'html_url' => $this->htmlUrl,
         ];
