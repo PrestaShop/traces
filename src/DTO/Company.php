de953d426a4fce581705809e1507e3b27799c4df
Progi1984 <franck@lefevre.dev>
2025-08-05 07:03:40 +0200

    Added TopCompanies based on year or on PS version

--- src/DTO/Company.php
+++ src/DTO/Company.php
@@ -7,6 +7,14 @@ class Company
     public int $mergedPullRequests = 0;
     public int $rank = 0;
     public float $pullRequestsPercent = 0;
+    /**
+     * @var array<int, int>
+     */
+    public array $mergedPullRequestsByYear = [];
+    /**
+     * @var array<string, int>
+     */
+    public array $mergedPullRequestsByVersion = [];
 
     public function __construct(
         public readonly string $name,
@@ -32,6 +40,8 @@ class Company
             'name' => $this->name,
             'rank' => $this->rank,
             'merged_pull_requests' => $this->mergedPullRequests,
+            'merged_pull_requests_by_version' => $this->mergedPullRequestsByVersion,
+            'merged_pull_requests_by_year' => $this->mergedPullRequestsByYear,
             'pull_requests_percent' => $this->pullRequestsPercent,
             'avatar_url' => $this->avatarUrl,
             'html_url' => $this->htmlUrl,
